c2e9c840413c975fd3b7a836ef689c762a509512
johan-t <johan@trieloff.net>
2022-12-07 23:27:53 +0100

    now logs in correctly

--- discord.js
+++ discord.js
@@ -10,14 +10,14 @@ const client = new Client({
 
 //console.log once client logs in
 client.once('ready', () => {
-    console.log('Logged in as ${client.user.tag}!');    
+    console.log('Logged in as '+ client.user.tag);    
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
 
 
 // register a new command with the name "ping"
-client.on('messageCreate', message => {
+client.on('message', message => {
     if (message.content === '!ping') {
         // send back "Pong." to the channel the message was sent in
         message.channel.send('Pong.');
