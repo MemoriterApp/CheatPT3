5a9ef30eb791611bae19edc7b35d124ec40641dd
johan-t <johan@trieloff.net>
2022-12-07 21:34:19 +0100

    basic stuff

--- discord.js
+++ discord.js
@@ -1,1 +1,10 @@
-//why does git not work?
+//Create a new Discord client object, which will be used to communicate with the Discord API:
+const Discord = require('discord.js');
+const client = new Discord.Client(); 
+
+//console.log once client logs in
+client.once('ready', () => {
+    console.log('Logged in as ${client.user.tag}!');
+});
+
+client.login('token');
