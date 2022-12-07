91c82f42fe4d54487ab2086ba345336f811653bb
johan-t <johan@trieloff.net>
2022-12-07 23:24:39 +0100

    bot is "running"

--- discord.js
+++ discord.js
@@ -1,10 +1,26 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require('discord.js')
 //Create a new Discord client object, which will be used to communicate with the Discord API:
-const Discord = require('discord.js');
-const client = new Discord.Client(); 
+const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+    ]
+    });
 
 //console.log once client logs in
 client.once('ready', () => {
-    console.log('Logged in as ${client.user.tag}!');
+    console.log('Logged in as ${client.user.tag}!');    
 });
 
-client.login('token');
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+
+// register a new command with the name "ping"
+client.on('messageCreate', message => {
+    if (message.content === '!ping') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('Pong.');
+    }
+});
+
