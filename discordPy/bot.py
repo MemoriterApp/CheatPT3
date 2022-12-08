5123272bd1706df795eb46d836eec2b2bac60448
johan-t <johan@trieloff.net>
2022-12-08 22:53:03 +0100

    bot now responds!!!

--- discordPy/bot.py
+++ discordPy/bot.py
@@ -7,10 +7,18 @@ load_dotenv() # load the .env file
 
 TOKEN = os.getenv('DISCORD_TOKEN') # get the token from the .env file
 
-client = discord.Client(intents=discord.Intents.default()) # a client is a connection to Discord
+client = discord.Client(intents=discord.Intents.all()) # a client is a connection to Discord
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'hello':
+        await message.channel.send('Hello!')
 
 client.run(TOKEN)
