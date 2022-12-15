b93157ab10cabc1be2a2692b408934c38940125f
johan-t <johan@trieloff.net>
2022-12-15 21:49:01 +0100

    chaaanges

--- commands/study.js
+++ commands/study.js
@@ -1,4 +1,12 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { Configuration, OpenAIApi } = require ('openai');
+const { APIKey } = require('../config.json');
+
+// Set up the OpenAI API client
+const configuration = new Configuration({
+	apiKey: APIKey,
+});
+const openai = new OpenAIApi(configuration);
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,6 +18,13 @@ module.exports = {
 				.setRequired(true)),
 	async execute(interaction) {
 		const AiPrompt = interaction.options.getString('question');
-		await interaction.reply('You asked: ' + AiPrompt + '');
+		// Use the OpenAI API to generate a response to the given prompt
+		const response = await openai.createCompletion({
+			model: 'text-ada-001',
+			prompt: AiPrompt,
+			temperature: 0.25,
+		});
+		// Send the response back to the user
+		await interaction.reply(response.data.choices[0].text);
 	},
 };
